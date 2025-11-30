ecc142ede08460141e3f4c96dd53e5d4b6062b05
jreverdy <patchzi01@hotmail.fr>
2025-11-30 14:07:26 +0100

    wip

---
+++ level5/source.c
@@ -0,0 +1,28 @@
+
+void o(void)
+
+{
+  system("/bin/sh");
+                    /* WARNING: Subroutine does not return */
+  _exit(1);
+}
+
+
+
+void n(void)
+
+{
+  char local_20c [520];
+  
+  fgets(local_20c,0x200,stdin);
+  printf(local_20c);
+  exit(1);
+}
+
+void main(void)
+
+{
+  n();
+  return;
+}
+
